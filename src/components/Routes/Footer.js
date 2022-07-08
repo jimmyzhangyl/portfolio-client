b4090a860bc9b8292f34705e825354fdb0ee4da5
Yuanlong Zhang <jimzhangyl@gmail.com>
2022-07-08 17:14:31 +1000

    add GitHub Link

--- src/components/Routes/Footer.js
+++ src/components/Routes/Footer.js
@@ -1,11 +1,15 @@
 
 export default function Footer() {
     const LinkedIn = "https://www.linkedin.com/in/yuanlong-zhang";
+    const GitHub = "https://github.com/jimmyzhangyl"; 
     return (
         <footer className="bg-light relative m-1 text-center nav-font">
             <h5>Find me on:&nbsp;
                 <a href={LinkedIn} target="_blank" rel="noreferrer">
-                    <img class="pb-1 pt-1" width="20" src={require("../../img/LinkedIn.png")} alt="LinkedIn" />
+                    <img class="py-1 mx-3" width="25px" src={require("../../img/LinkedIn.png")} alt="LinkedIn" />
+                </a>
+                <a href={GitHub} target="_blank" rel="noreferrer">
+                    <img class="py-1 mx-3" width="25px" src={require("../../img/GitHub.png")} alt="LinkedIn" />
                 </a>
             </h5>
         </footer >
