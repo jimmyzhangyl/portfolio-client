bd93f97ad3809da3dfd613e880b3fafb3c241c43
Yuanlong Zhang <jimzhangyl@gmail.com>
2022-07-09 19:42:20 +1000

    change browerhistory route to hashhistory route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,34 @@
 import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter,HashRouter, Route, Routes } from "react-router-dom";
 import { Navigation, Footer, About, Skills, Projects, Contact, ProjectPage } from "../components";
+import { createBrowserHistory } from "history";
+
 
 
 function App() {
+  const customHistory = createBrowserHistory();
   const root = process.env.REACT_APP_ROOT;
   useEffect(() => {
     document.title = "YL-Z Portfolio"
   }, []);
 
   return (
-    <BrowserRouter>
+    <HashRouter >
       <Navigation />
       <Routes>
-        <Route path={`${root}/`} element={<About />} />
-        <Route path={`${root}/home`} element={<About />} />
-        <Route path={`${root}/about`} element={<About />} />
-        <Route path={`${root}/skills`} element={<Skills />} />
-        <Route path={`${root}/projects`} element={<Projects />}>
+      <Route path={`/`} element={<About />} />
+        <Route path={`/${root}/`} element={<About />} />
+        <Route path={`/${root}/home`} element={<About />} />
+        <Route path={`/${root}/about`} element={<About />} />
+        <Route path={`/${root}/skills`} element={<Skills />} />
+        <Route path={`/${root}/projects`} element={<Projects />}>
           <Route path=":project" element={<ProjectPage />} />
         </Route>
-        <Route path={`${root}/contact`} element={<Contact />} />
+        <Route path={`/${root}/contact`} element={<Contact />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
