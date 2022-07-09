5196ad6a5ea785fc68d98e85fb6ecdb16568ddbe
Yuanlong Zhang <jimzhangyl@gmail.com>
2022-07-09 18:52:00 +1000

    fix deploy routing issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,27 @@
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navigation, Footer, About, Skills, Projects, Contact, ProjectPage } from "../components";
 
 
 function App() {
+  const root = process.env.REACT_APP_ROOT;
+  useEffect(() => {
+    document.title = "YL-Z Portfolio"
+  }, []);
+
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/home" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />}>
+        <Route path={`${root}/`} element={<About />} />
+        <Route path={`${root}/home`} element={<About />} />
+        <Route path={`${root}/about`} element={<About />} />
+        <Route path={`${root}/skills`} element={<Skills />} />
+        <Route path={`${root}/projects`} element={<Projects />}>
           <Route path=":project" element={<ProjectPage />} />
         </Route>
-        <Route path="/contact" element={<Contact />} />
+        <Route path={`${root}/contact`} element={<Contact />} />
       </Routes>
       <Footer />
     </BrowserRouter>
